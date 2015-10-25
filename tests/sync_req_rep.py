3454094f863815311b03d15909730c172e392a8c
Bitworks Software, Ltd <bitworks@bw-sw.com>
2015-10-25 15:14:11 +0500

    Update sync_req_rep.py

--- tests/sync_req_rep.py
+++ tests/sync_req_rep.py
@@ -19,7 +19,7 @@ from   moebius import *
 #-------------------------------------------------------------
 # specific client which is derived from basic REQ/REP client
 #
-class Client(utils.ZMQClient):
+class Client(utils.ReqRepClient):
     def send(self, message):
         super(Client, self).send(message=message)
 
